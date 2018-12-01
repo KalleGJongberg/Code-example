5b7e11ba2afc6a454c149d7ea5231c08e4d6e3f3
Kalle <43696198+lil-kalle@users.noreply.github.com>
2018-12-01 18:06:19 +0100

    kommentar og hjælpe funkion

--- binearseache.c
+++ binearseache.c
@@ -1,33 +1,41 @@
+/*
+Aouther Kalle Jongberg
+written 1/12 2018
+This is a program there uses binary seach to chek i an element is in an arry.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-int binearseache(int *arr, int seach_n, int min, int max);
-int main(void){
-  int* arr = malloc(sizeof(int)*5);
-  for (size_t i = 0; i < 5; i++) {
-    arr[i] = i;
-  }
+int binearseache_help(int *arr, int seach_n, int min, int max); // Prototype of the helping funktion
 
-int seach_n = 6;
-int min = -1;
-int max = 5;
-printf("%d\n", binearseache(arr,seach_n,min,max));
+// main binearseache funktion
+int binearseache(int *arr, int seach_n,int n){
+  // computing input for the heloing funktion.
+  int min = 0;
+  int max  = n -1;
+  // returning if the element where found and in which place.
+  // If the element where not in the array -1 is returend.
+return (binearseache_help(arr,seach_n, min, max));
 }
-
-
-int binearseache(int *arr, int seach_n, int min, int max){
+// this is the helping funktion for the bineary seach.
+int binearseache_help(int *arr, int seach_n, int min, int max){
+  // cheking if the input is legal.
   if (min < 0) {return -1;}
-  if(max > (sizeof(*arr)/sizeof(arr[0]))){
-    return -1;
-  }
+  // calculationg the midel of the array.
 int middel = min+(max-min)/2;
+// cheking if the midel element is the whished value.
 if(arr[middel] == seach_n){return middel;}
+// Cheking that the index is't wrong.
 else if(min >= max ){return -1;}
 else{
+  // cheking if the number is smaler than the midel value.
   if(arr[middel] > seach_n){
-    return (binearseache(arr, seach_n,min, middel-1));
+    // if it is smaler is the helping funktion called rekusiv on the small half of the array.
+    return (binearseache_help(arr, seach_n,min, middel-1));
   }
   else{
-    return (binearseache(arr, seach_n,middel+1, max));
+    // else is the helping funktion called rekusiv on the upper half of the array.
+    return (binearseache_help(arr, seach_n,middel+1, max));
   }
 }
 }
